a5a3dbd048917dce3713293aca7ce35246712d53
内田正輝 <masaki.u@arsaga.jp>
2021-08-12 16:36:36 +0900

    ページング処理の基本実装を作成

--- src/main/java/jp/co/arsaga/extensions/repository/common/BaseRepository.kt
+++ src/main/java/jp/co/arsaga/extensions/repository/common/BaseRepository.kt
@@ -1,5 +1,7 @@
 package jp.co.arsaga.extensions.repository.common
 
+import java.util.concurrent.atomic.AtomicBoolean
+
 interface BaseRepository<Store, Req> {
     val dataSource: Store
     fun refresh()
@@ -8,7 +10,8 @@ interface BaseRepository<Store, Req> {
 
     abstract class Impl<Res, Store, Req>(initRequest: Req?) : BaseRepository<Store, Req> {
 
-        private var latestRequestCache: Req? = initRequest
+        internal var latestRequestCache: Req? = initRequest
+            private set
 
         override fun refresh() {
             dispatch(latestRequestCache)
@@ -24,5 +27,46 @@ interface BaseRepository<Store, Req> {
         protected abstract fun dataPush(response: Res?)
 
         protected abstract fun dispatch(request: Req?)
+    }
+}
+
+interface BasePagingRepository<Store, Req> : BaseRepository<Store, Req> {
+
+    val isBusy: AtomicBoolean
+
+    fun fetchNextPage()
+
+    fun release()
+
+    fun isTerminal(): Boolean
+
+    abstract class Impl<Res, Store, Req, Content>(
+        initRequest: Req?
+    ) : BaseRepository.Impl<Res, Store, Req>(initRequest), BasePagingRepository<Store, Req> {
+
+        abstract fun limitEntityCount(): Int
+
+        abstract fun currentList(): Collection<Content>
+
+        abstract fun combineList(currentList: Collection<Content>, response: Res): Res
+
+        abstract fun nextRequestFactory(offset: Int, latestRequest: Req?): Req?
+
+        override val isBusy: AtomicBoolean = AtomicBoolean(false)
+
+        override fun fetch(request: Req?) {
+            if (!isTerminal() && isBusy.compareAndSet(false, true)) {
+                super.fetch(request)
+            }
+        }
+
+        override fun fetchNextPage() {
+            nextRequestFactory(currentList().size.plus(1), latestRequestCache)
+                ?.run(::fetch)
+        }
+
+        override fun release() {
+            isBusy.set(false)
+        }
     }
 }
