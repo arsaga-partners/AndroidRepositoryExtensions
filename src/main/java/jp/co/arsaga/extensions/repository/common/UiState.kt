dd5c836a5285360efbc74ba09e4c34017d88b550
内田正輝 <masaki.u@arsaga.jp>
2021-09-01 17:17:19 +0900

    フェッチの状態を表現する型を作成

---
+++ src/main/java/jp/co/arsaga/extensions/repository/common/UiState.kt
@@ -0,0 +1,34 @@
+package jp.co.arsaga.extensions.repository.common
+
+/**
+ * IOから取得する処理で、データとは別にをエラー状況と読み込み中の状況を個別に管理できるクラス
+ * 画面に最終結果・読み込み中・エラーのそれぞれの状態を表示するのに便利
+ * 主にGET系の処理でRepositoryとセットで使う
+ */
+data class UiState<T>(
+    val loading: Boolean = false,
+    val exception: Exception? = null,
+    val data: T? = null
+) {
+    /**
+     * エラーかどうか
+     */
+    val hasError: Boolean
+        get() = exception != null
+
+    /**
+     * 初回読み込み中かどうか
+     */
+    val initialLoad: Boolean
+        get() = data == null && loading && !hasError
+}
+
+/**
+ * IOの成功/失敗時の値渡しの処理を抽象化したもの
+ */
+fun <T> UiState<T>.copyWithResult(
+    value: Result<T>
+): UiState<T> = when (value) {
+    is Result.Success -> copy(loading = false, exception = null, data = value.data)
+    is Result.Error -> copy(loading = false, exception = value.exception)
+}
