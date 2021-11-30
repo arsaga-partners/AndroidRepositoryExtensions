c70b66e5d4beeec3e84562bda595604dc740eec6
内田正輝 <masaki.u@arsaga.jp>
2021-11-30 20:28:47 +0900

    IOActionEntityの管理を楽にするクラスを作成

--- src/main/java/jp/co/arsaga/extensions/repository/common/IOActionEntity.kt
+++ src/main/java/jp/co/arsaga/extensions/repository/common/IOActionEntity.kt
@@ -1,5 +1,9 @@
 package jp.co.arsaga.extensions.repository.common
 
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
+
 /**
  * UiStateで画面状態をハンドリングする際、Viewの受け取る引数が
  * (uiStateA, uiStateB..., sendActionA, sendActionB...)となるのを
@@ -13,7 +17,19 @@ package jp.co.arsaga.extensions.repository.common
  * @constructor result IOの返り値が入るプロパティ
  * @constructor send IO処理を実行するラムダ
  */
-data class IOActionEntity<T>(val result: UiState<T>?, val send: () -> Unit)
+data class IOActionEntity<T>(val result: UiState<T>?, val send: () -> Unit) {
+    class Factory<T>(
+        private val sendFactory: ((UiState<T>) -> Unit) -> Unit
+    ) {
+        private val _status = MutableStateFlow(IOActionEntity<T>(null, ::sendAction))
+        val status: StateFlow<IOActionEntity<T>> = _status
+        private fun sendAction() {
+            sendFactory { res ->
+                _status.update { it.copy(result = res) }
+            }
+        }
+    }
+}
 
 /**
  * 1つのAPIに対して複数のボタンから複数の叩き方をする想定のクラス
