7985ea52f5191b55ce3892ff9f81e366c174ffca
内田正輝 <masaki.u@arsaga.jp>
2022-03-07 11:18:28 +0900

    アクセス解析の基本型を作成

---
+++ src/main/java/jp/co/arsaga/extensions/repository/common/AnalyticsInterface.kt
@@ -0,0 +1,30 @@
+package jp.co.arsaga.extensions.repository.common
+
+import android.content.Context
+import androidx.annotation.StringRes
+import kotlinx.coroutines.flow.Flow
+
+@JvmInline
+value class AnalyticsStringResource(
+    @StringRes val resourceId: Int
+)
+
+interface BaseAnalyticsDispatcher<T : Enum<T>> {
+    fun post(
+        context: Context,
+        analyticsDataFactory: BaseAnalyticsDataFactory<T>,
+    )
+}
+
+interface BaseAnalyticsDataFactory<AnalyticsEventType : Enum<AnalyticsEventType>> {
+    val analyticsEventType: AnalyticsEventType
+    fun create(): MutableMap<String, Any>?
+}
+
+interface BaseAnalyticsFragment<T : Enum<T>> {
+    val analyticsDataFactory: Flow<BaseAnalyticsDataFactory<T>?>
+}
+
+interface BaseAnalyticsAction<T : Enum<T>> {
+    val analyticsAction: (BaseAnalyticsDataFactory<T>) -> Unit
+}
